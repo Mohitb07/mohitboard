8de3afa428309e5cec5d8639407da23a4661ca44
Mohitb07 <bmohit980@gmail.com>
2020-07-23 12:51:27 +0530

    added unique constraint to email field

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
-    class Meta:
+    class Meta(object):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean(self):
+       email = self.cleaned_data.get('email')
+       if User.objects.filter(email=email).exists():
+           raise forms.ValidationError('Email already in use')
+       return self.cleaned_data
